812498195620b06738768f765caa3aae399322c5
Renée Kooi <renee@kooi.me>
2018-09-22 18:24:03 +0200

    Report autofix possibilities

--- src/cli_reporter.rs
+++ src/cli_reporter.rs
@@ -47,8 +47,24 @@ fn format_note(source: &str, note: &Note) -> String {
 }
 
 pub fn report(source: &str, warnings: Vec<Warning>) -> () {
+    let mut num_warnings = 0;
+    let mut num_errors = 0;
+    let mut fixable_warnings = 0;
+    let mut fixable_errors = 0;
+
     for warn in warnings {
         let offending_line = warn.start().line();
+
+        match warn.severity() {
+            Severity::Error => num_errors += 1,
+            Severity::Warning => num_warnings += 1,
+        }
+        if warn.suggestions().iter().any(|s| s.replacement().is_some()) {
+            match warn.severity() {
+                Severity::Error => fixable_errors += 1,
+                Severity::Warning => fixable_warnings += 1,
+            }
+        }
 
         println!("\n{}", format_message(&warn));
         for (n, line) in slice_lines(&source, warn.range(), 1) {
@@ -75,5 +91,11 @@ pub fn report(source: &str, warnings: Vec<Warning>) -> () {
         for suggestion in warn.suggestions() {
             println!("\n{}", indent(&format_suggestion(&suggestion), "    "));
         }
+    }
+
+    println!("");
+    println!("{} errors, {} warnings found.", num_errors, num_warnings);
+    if fixable_errors > 0 || fixable_warnings > 0 {
+        println!("{} errors, {} warnings fixable using --fix", fixable_errors, fixable_warnings);
     }
 }
