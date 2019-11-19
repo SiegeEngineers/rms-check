25949e865e506ad287741afc4dc0ccd6455c2e62
Renée Kooi <renee@kooi.me>
2019-11-19 15:08:20 +0100

    lsp: mark comment folds as such

--- crates/rms-check-lsp/src/folds.rs
+++ crates/rms-check-lsp/src/folds.rs
@@ -1,18 +1,7 @@
 use codespan::{ByteIndex, ByteOffset, FileId, Files, Location};
-use lsp_types::FoldingRange;
+use lsp_types::{FoldingRange, FoldingRangeKind};
 use rms_check::{Atom, Parser};
 use std::ops::{Bound, RangeBounds};
-
-/// FoldingRange doesn't derive Default...
-fn default_fold() -> FoldingRange {
-    FoldingRange {
-        start_line: 0,
-        end_line: 0,
-        start_character: Default::default(),
-        end_character: Default::default(),
-        kind: Default::default(),
-    }
-}
 
 #[derive(Debug)]
 pub struct FoldingRanges<'a> {
@@ -46,7 +35,7 @@ impl<'a> FoldingRanges<'a> {
         self.queued.push(range);
     }
 
-    fn fold_lines(&mut self, range: impl RangeBounds<ByteIndex>) {
+    fn fold_lines(&mut self, range: impl RangeBounds<ByteIndex>, kind: Option<FoldingRangeKind>) {
         let start_line = match range.start_bound() {
             Bound::Unbounded => 0u64,
             Bound::Included(index) => self.line(*index),
@@ -61,12 +50,14 @@ impl<'a> FoldingRanges<'a> {
             self.push(FoldingRange {
                 start_line,
                 end_line,
-                ..default_fold()
+                start_character: Default::default(),
+                end_character: Default::default(),
+                kind,
             });
         }
     }
 
-    fn fold(&mut self, range: impl RangeBounds<ByteIndex>) {
+    fn fold(&mut self, range: impl RangeBounds<ByteIndex>, kind: Option<FoldingRangeKind>) {
         let (start_line, start_character) = match range.start_bound() {
             Bound::Unbounded => (0u64, 0u64),
             Bound::Included(index) => {
@@ -98,7 +89,7 @@ impl<'a> FoldingRanges<'a> {
             end_line,
             start_character: Some(start_character),
             end_character: Some(end_character),
-            kind: Default::default(),
+            kind,
         });
     }
 }
@@ -117,12 +108,12 @@ impl Iterator for FoldingRanges<'_> {
         };
         match atom {
             Comment(start, _, Some(end)) => {
-                self.fold_lines(start.span.start()..=end.span.start());
+                self.fold_lines(start.span.start()..=end.span.start(), Some(FoldingRangeKind::Comment));
             }
             OpenBlock(_) => self.waiting_folds.push(atom),
             CloseBlock(end) => {
                 if let Some(OpenBlock(start)) = self.waiting_folds.pop() {
-                    self.fold(start.span.end()..end.span.start());
+                    self.fold(start.span.end()..end.span.start(), None);
                 }
             }
             If(_, _) => self.waiting_folds.push(atom),
@@ -131,12 +122,12 @@ impl Iterator for FoldingRanges<'_> {
                     Some(If(start, _)) | Some(ElseIf(start, _)) => start,
                     _ => return self.next(),
                 };
-                self.fold_lines(start.span.start()..end.span.start());
+                self.fold_lines(start.span.start()..end.span.start(), None);
                 self.waiting_folds.push(atom);
             }
             EndIf(end) => match self.waiting_folds.pop() {
                 Some(If(start, _)) | Some(ElseIf(start, _)) | Some(Else(start)) => {
-                    self.fold_lines(start.span.start()..=end.span.start());
+                    self.fold_lines(start.span.start()..=end.span.start(), None);
                 }
                 _ => (),
             },
@@ -144,7 +135,7 @@ impl Iterator for FoldingRanges<'_> {
             PercentChance(end, _) => {
                 if let Some(PercentChance(start, _)) = self.waiting_folds.last() {
                     let start = start.span.start();
-                    self.fold_lines(start..end.span.start());
+                    self.fold_lines(start..end.span.start(), None);
                     self.waiting_folds.pop();
                 }
                 self.waiting_folds.push(atom);
@@ -152,12 +143,12 @@ impl Iterator for FoldingRanges<'_> {
             EndRandom(end) => {
                 if let Some(PercentChance(start, _)) = self.waiting_folds.last() {
                     let start = start.span.start();
-                    self.fold_lines(start..end.span.start());
+                    self.fold_lines(start..end.span.start(), None);
                     self.waiting_folds.pop();
                 }
                 if let Some(StartRandom(start)) = self.waiting_folds.last() {
                     let start = start.span.start();
-                    self.fold_lines(start..=end.span.start());
+                    self.fold_lines(start..=end.span.start(), None);
                     self.waiting_folds.pop();
                 }
             }
