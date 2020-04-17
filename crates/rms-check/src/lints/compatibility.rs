34f78cd6cf79e146ac53664bd47ff16a46bf68e6
Renée Kooi <renee@kooi.me>
2020-04-17 15:04:47 +0200

    Allow nomad_resources on HD Edition

--- crates/rms-check/src/lints/compatibility.rs
+++ crates/rms-check/src/lints/compatibility.rs
@@ -47,7 +47,7 @@ impl Lint for CompatibilityLint {
                 }
                 "direct_placement" => {
                     if !self.has_up_extension(state) {
-                        warnings.push(Diagnostic::warning(atom.location, "Direct placement requires UserPatch 1.5")
+                        warnings.push(Diagnostic::warning(atom.location, "Direct placement requires UserPatch 1.5 or Definitive Edition")
                                 .suggest(Fix::new(atom.location,
                                     "Wrap this command in an `if UP_EXTENSION` statement or add a /* Compatibility: UserPatch 1.5 */ comment at the top of the file",
                                 ))
@@ -55,9 +55,9 @@ impl Lint for CompatibilityLint {
                     }
                 }
                 "nomad_resources" => {
-                    if !self.has_up_available(state) {
+                    if !self.has_up_available(state) && state.compatibility() != Compatibility::HDEdition {
                         warnings.push(
-                            Diagnostic::warning(atom.location, "Nomad resources requires UserPatch 1.4")
+                            Diagnostic::warning(atom.location, "Nomad resources requires UserPatch 1.4 or HD Edition")
                                 .suggest(Fix::new(atom.location,
                                     "Wrap this command in an `if UP_AVAILABLE` statement or add a /* Compatibility: UserPatch 1.4 */ comment at the top of the file",
                                 ))
@@ -68,15 +68,13 @@ impl Lint for CompatibilityLint {
                 | "actor_area_to_place_in"
                 | "avoid_actor_area"
                 | "avoid_all_actor_areas"
-                | "actor_area_radius" => {
-                    if state.compatibility() != Compatibility::DefinitiveEdition {
-                        warnings.push(
-                            Diagnostic::warning(atom.location, "Actor areas are only supported in the Definitive Edition")
-                                .suggest(Fix::new(atom.location,
-                                    "Add a /* Compatibility: Definitive Edition */ comment at the top of the file",)
+                | "actor_area_radius" if state.compatibility() != Compatibility::DefinitiveEdition => {
+                    warnings.push(
+                        Diagnostic::warning(atom.location, "Actor areas are only supported in the Definitive Edition")
+                        .suggest(Fix::new(atom.location,
+                                          "Add a /* Compatibility: Definitive Edition */ comment at the top of the file",)
                                 )
                         )
-                    }
                 }
                 "avoid_forest_zone"
                 | "place_on_forest_zone"
@@ -88,15 +86,13 @@ impl Lint for CompatibilityLint {
                             )
                     )
                 }
-                "second_object" => {
-                    if state.compatibility() != Compatibility::DefinitiveEdition {
-                        warnings.push(
-                            Diagnostic::warning(atom.location, "second_object is only supported in the Definitive Edition")
-                                .suggest(Fix::new(atom.location,
-                                    "Add a /* Compatibility: Definitive Edition */ comment at the top of the file",)
-                                )
-                        )
-                    }
+                "second_object" if state.compatibility() != Compatibility::DefinitiveEdition => {
+                    warnings.push(
+                        Diagnostic::warning(atom.location, "second_object is only supported in the Definitive Edition")
+                            .suggest(Fix::new(atom.location,
+                                "Add a /* Compatibility: Definitive Edition */ comment at the top of the file",)
+                            )
+                    )
                 }
                 _ => (),
             }
@@ -126,11 +122,11 @@ impl Lint for CompatibilityLint {
 #[cfg(test)]
 mod tests {
     use super::CompatibilityLint;
-    use crate::{RMSCheck, RMSFile, Severity};
+    use crate::{Compatibility, RMSCheck, RMSFile, Severity};
 
     #[test]
-    fn compatibility() {
-        let file = RMSFile::from_path("./tests/rms/compatibility.rms").unwrap();
+    fn compatibility() -> anyhow::Result<()> {
+        let file = RMSFile::from_path("./tests/rms/compatibility.rms")?;
         let result = RMSCheck::new()
             .with_lint(Box::new(CompatibilityLint::new()))
             .check(&file);
@@ -141,5 +137,38 @@ mod tests {
         assert_eq!(first.severity(), Severity::Warning);
         assert_eq!(first.code(), Some("compatibility"));
         assert_eq!(first.message(), "RMS Effects require UserPatch 1.5");
+        Ok(())
+    }
+
+    #[test]
+    fn nomad_resources() -> anyhow::Result<()> {
+        let file = RMSFile::from_string("nomad.rms", "<PLAYER_SETUP> nomad_resources");
+
+        let checks = [
+            (Compatibility::All, false),
+            (Compatibility::HDEdition, true),
+            (Compatibility::UserPatch14, true),
+            (Compatibility::DefinitiveEdition, true),
+        ];
+
+        for (compatibility, supports_nomad_resources) in &checks {
+            let result = RMSCheck::new()
+                .compatibility(*compatibility)
+                .with_lint(Box::new(CompatibilityLint::new()))
+                .check(&file);
+
+            let mut warnings = result.iter();
+            if *supports_nomad_resources {
+                assert!(warnings.next().is_none());
+            } else {
+                // No compatibility setting: nomad_resources may be unsupported
+                let first = warnings.next().unwrap();
+                assert!(warnings.next().is_none());
+                assert_eq!(first.severity(), Severity::Warning);
+                assert_eq!(first.code(), Some("compatibility"));
+            }
+        }
+
+        Ok(())
     }
 }
